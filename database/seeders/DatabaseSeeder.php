80c743aeb6ab3648814862c7b71011ea99ea0cce
AnilOnayy <cengizhananilonay@gmail.com>
2023-10-05 00:12:32 +0300

    Seed Datas Added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+        
+        $user = User::factory()->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $personal  = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => 'My Excerpt',
+            'category_id' => $work->id,
+            'user_id' => $user->id,
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi, unde officiis assumenda possimus sit aspernatur inventore, distinctio commodi error dicta dolorem ea at maiores odit enim nihil voluptatibus dignissimos quis?</p>'
+        ]);
     }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <title>My Blog</title>
     <link rel="stylesheet" href="/css/app.css">
-    <script src="./js/app.js"></script>
+    <script src="/js/app.js"></script>
 </head>
 
 <body>
